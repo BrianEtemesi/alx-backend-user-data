17ff5d8c51b83d9a0869795b1980e9c176360f9d
BrianEtemesi <etemecci@gmail.com>
2023-08-08 10:13:50 +0000

    annotate fields with List[str]

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 implementation of a log filter
 """
 import re
+from typing import List
 
 
-def filter_datum(fields: list,
+def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """
     returns a log message obfuscated
