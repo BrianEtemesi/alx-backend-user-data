c27b451521e6600f06c8dde862d2a3a9166bd4d7
BrianEtemesi <etemecci@gmail.com>
2023-08-10 08:20:50 +0000

    use getenv

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -79,10 +79,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     returns a connector to a mysql database
     """
     config = {
-        "user": os.environ.get("PERSONAL_DATA_DB_USERNAME"),
-        "password": os.environ.get("PERSONAL_DATA_DB_PASSWORD"),
-        "host": os.environ.get("PERSONAL_DATA_DB_HOST"),
-        "database": os.environ.get("PERSONAL_DATA_DB_NAME")
+        "user": os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+        "password": os.getenv("PERSONAL_DATA_DB_PASSWORD"),
+        "host": os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        "database": os.getenv("PERSONAL_DATA_DB_NAME")
     }
 
     try:
