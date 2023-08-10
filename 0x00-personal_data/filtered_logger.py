be2f711db71f431a3ef5b43218861acfdce5ae27
BrianEtemesi <etemecci@gmail.com>
2023-08-10 08:31:20 +0000

    pycode error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -79,10 +79,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     returns a connector to a mysql database
     """
     connection = mysql.connector.connect(
-        user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password = os.getenv("PERSONAL_DATA_DB_PASSWORD"),
-        host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        database = os.getenv("PERSONAL_DATA_DB_NAME")
+        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.getenv("PERSONAL_DATA_DB_PASSWORD"),
+        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
     )
 
     return connection
