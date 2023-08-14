fa8e5411a774bff558c29e9d949a6c23d2458a64
BrianEtemesi <etemecci@gmail.com>
2023-08-14 16:22:52 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+implementation of a basic authentication class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    basic authentiction class that inherits from Auth
+    """
