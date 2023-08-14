fa082a66009bfc2ea375930e0e4df98bac743022
BrianEtemesi <etemecci@gmail.com>
2023-08-14 20:44:16 +0000

    basic base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 implementation of a basic authentication class
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,4 +23,22 @@ class BasicAuth(Auth):
         if authorization_header.startswith("Basic "):
             return authorization_header[len("Basic "):]
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        decodes the value of a base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except (binascii.Error, UnicodeDecodeError):
             return None
