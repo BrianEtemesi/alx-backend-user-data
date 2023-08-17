8bdbd9fe30bff7812b2f2cc2eae5348290b4b59e
BrianEtemesi <etemecci@gmail.com>
2023-08-17 14:28:23 +0000

    get _my_session_id from env variable

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ implementation of a class to manage authentication
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -57,4 +58,5 @@ class Auth:
         if request is None:
             return None
 
-        return request.cookies.get("_my_session_id")
+        _my_session_id = os.getenv('SESSION_NAME')
+        return request.cookies.get(_my_session_id)
