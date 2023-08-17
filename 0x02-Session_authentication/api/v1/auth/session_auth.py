b27213f591a2211d78e27995c9576b2aee3459aa
BrianEtemesi <etemecci@gmail.com>
2023-08-17 13:16:22 +0000

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+implementation of a session class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    class representation of session auth
+    """
