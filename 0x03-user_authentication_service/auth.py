ee8df1e18216354d37da6fa491882f143b70e5ae
BrianEtemesi <etemecci@gmail.com>
2023-09-28 17:22:44 +0000

    define auth method to destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,3 +98,10 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        destroys a user's session
+        """
+        self._db.update_user(user_id, session_id=None)
+        return None
