cfd28f61fb1fb36802bed16cd59231414a9ecb93
BrianEtemesi <etemecci@gmail.com>
2023-09-28 09:46:00 +0000

    register user on server endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 flask application
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -13,8 +15,26 @@ def payload():
     """
     returns  json payload
     """
-    data = {"message": "Bienvenue"}
-    return jsonify(data)
+    response_data = {"message": "Bienvenue"}
+    return jsonify(response_data)
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    registers a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+        response_data = {"email": "{}".format(email),
+                         "message": "user created"}
+        return jsonify(response_data)
+    except ValueError:
+        response_data = {"message": "email already registered"}
+        return jsonify(response_data), 400
 
 
 if __name__ == "__main__":
