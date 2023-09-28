56bcffd399c492bea2cda53df6a976e8fa2abf5a
BrianEtemesi <etemecci@gmail.com>
2023-09-28 10:13:03 +0000

    define method to validate credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,20 @@ class Auth:
             user = self._db.add_user(email, hashed_password)
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        validates user credentials
+        """
+        # try locating user by email
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                # encode entered password
+                encoded_pw = password.encode('utf-8')
+                # compare entered pwd with user pwd
+                return bcrypt.checkpw(encoded_pw, user.hashed_password)
+            else:
+                return False
+        except NoResultFound:
+            return False
