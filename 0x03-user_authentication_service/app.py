25174f9155c6bda3d32fc6d88b08cd7191921de7
BrianEtemesi <etemecci@gmail.com>
2023-09-28 18:15:23 +0000

    add check for cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,14 +66,17 @@ def logout():
     # retrieve session id from request cookie
     session_id = request.cookies.get('session_id')
 
+    if session_id is None:
+        abort(403)
+
     # find user with retrieved session id
     user = AUTH.get_user_from_session_id(session_id)
 
-    if user is not None:
+    if user:
         AUTH.destroy_session(user.id)
         return redirect(url_for('payload'))
     else:
-        return 403
+        abort(403)
 
 
 if __name__ == "__main__":
